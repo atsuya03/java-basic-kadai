1accafc0a86e01bf7b14987c103ba8fa1e6f4f2e
atsuya03 <147512419+atsuya03@users.noreply.github.com>
2023-10-27 23:18:12 +0900

    Add files via upload

---
+++ kadai_020/DictionaryExec_Chapter21.java
@@ -0,0 +1,25 @@
+package kadai_020;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Dictionary_Chapter20 search = new Dictionary_Chapter20();
+		
+		
+		//単語を追加
+		search.addWord();
+		search.getMeaning("apple");
+		search.getMeaning("banana");
+		search.getMeaning("grape");
+		search.getMeaning("orange");
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ kadai_020/Dictionary_Chapter20.java
@@ -0,0 +1,83 @@
+package kadai_020;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Dictionary_Chapter20 {
+	HashMap<String,String> dicMap = new HashMap<>();
+	
+	//単語の追加
+		public void addWord() {
+		
+		dicMap.put("apple", "りんご");
+		dicMap.put("peach", "桃");
+		dicMap.put("banana", "バナナ");
+		dicMap.put("lemon", "レモン");
+		dicMap.put("pear", "梨");
+		dicMap.put("kiwi", "キウイ");
+		dicMap.put("strawberry", "いちご");
+		dicMap.put("grape", "ぶどう");
+		dicMap.put("muscat", "マスカット");
+		dicMap.put("cherry", "さくらんぼ");
+		
+		}
+		
+	//単語の検索
+		 public  void getMeaning(String searchWord) {
+			 Set<String> words =dicMap.keySet();
+			 
+			 for(String key:words) {
+				 if(key.equals(searchWord)) {
+				 String meaning = dicMap.get(key);
+
+				System.out.println(searchWord+"意味は"+meaning+"です。");
+				return;
+				 }
+			 }
+					 System.out.println(searchWord+"は辞書に存在しません。");
+				 }
+			 
+		 }
+
+				
+		    
+		
+		//単語
+//		public void research() {
+//		for(String key : dicMap.keySet()) {
+//			if(dicMap.containsKey(key)) {
+//			
+//			System.out.println(key+"の意味は"+dicMap.get(key)+"です");
+//	
+//			}else {
+//				System.out.println(key+"は存在しません");
+//			}
+//			
+//			}
+//		
+//			
+//		}
+//		public String getMeaning(String word) {
+//			return dicMap.get(word);
+			
+	
+//			System.out.println("peachの意味は"+dicMap.get("peach")+"です");
+//			System.out.println("bananaの意味は"+dicMap.get("banana")+"です");
+//			System.out.println("lemonの意味は"+dicMap.get("lemon")+"です");
+//			System.out.println("pearの意味は"+dicMap.get("pear")+"です");
+//			System.out.println("kiwiの意味は"+dicMap.get("kiwi")+"です");
+//			System.out.println("strawberryの意味は"+dicMap.get("strawberry")+"です");
+//			System.out.println("grapeの意味は"+dicMap.get("grape")+"です");
+//			System.out.println("muscatの意味は"+dicMap.get("muscat")+"です");
+//			System.out.println("cherryの意味は"+dicMap.get("cherry")+"です");
+			
+			
+			
+			
+			
+		
+		
+		
+
+
+
